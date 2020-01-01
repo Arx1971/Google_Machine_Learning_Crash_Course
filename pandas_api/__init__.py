824345484a2d0f4cad05274490670dd1e40d6539
Arx1971 <adnan.rahin13@gmail.com>
2020-01-01 13:28:12 -0500

    udpate

---
+++ pandas_api/__init__.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
+population = pd.Series([852469, 1015785, 485199])
+
+df = pd.DataFrame({'City Name': city_names, 'Population': population})
+
+california_housing_dataframe = pd.read_csv(
+    "https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
+
+plt = california_housing_dataframe.hist('housing_median_age')
+plt.show()
