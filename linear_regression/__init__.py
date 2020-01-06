8a1b0744f239ba9c8babac61dc497dba291dda1b
Arx1971 <adnan.rahin13@gmail.com>
2020-01-05 23:45:43 -0500

    Target is set

--- linear_regression/__init__.py
+++ linear_regression/__init__.py
@@ -7,9 +7,7 @@ from sklearn.metrics import mean_squared_error
 
 def data_preprocess(data):
     features = data.columns[data.isnull().mean() < 0.8]
-
     new_data = data[features]
-
     return new_data
 
 
@@ -18,3 +16,5 @@ test_data = pd.read_csv('test.csv')
 
 new_train_data = data_preprocess(train_data)
 new_test_data = data_preprocess(test_data)
+
+y = new_test_data['SalePrice']  # this is our target
