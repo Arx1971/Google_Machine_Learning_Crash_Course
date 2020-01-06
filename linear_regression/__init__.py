718a36981cd27d0f0775bcc4643bef4ffb4c10ed
Arx1971 <adnan.rahin13@gmail.com>
2020-01-05 21:18:13 -0500

    feature selected

--- linear_regression/__init__.py
+++ linear_regression/__init__.py
@@ -1,3 +1,24 @@
 import pandas as pd
 import numpy as np
 
+data = pd.read_csv('../house_prices_train.csv')
+
+print(data.columns)
+
+features = [
+    'MSSubClass',
+    'LotArea',
+    'LotFrontage',
+    'LotShape',
+    'OverallQual',
+    'OverallCond',
+    'BsmtUnfSF',
+    '1stFlrSF',
+    '2ndFlrSF',
+    'GrLivArea'
+]
+
+y = data['SalePrice']
+X = data[features]
+
+print(X)
