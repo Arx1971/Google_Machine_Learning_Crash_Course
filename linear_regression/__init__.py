3cbf2ae0bcda6238af1f9c74e2e4eeed8ca117ea
Arx1971 <adnan.rahin13@gmail.com>
2020-01-05 23:42:48 -0500

    Data Preprocess Done

--- linear_regression/__init__.py
+++ linear_regression/__init__.py
@@ -4,7 +4,17 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
+
+def data_preprocess(data):
+    features = data.columns[data.isnull().mean() < 0.8]
+
+    new_data = data[features]
+
+    return new_data
+
+
 train_data = pd.read_csv('train.csv')
 test_data = pd.read_csv('test.csv')
 
-
+new_train_data = data_preprocess(train_data)
+new_test_data = data_preprocess(test_data)
