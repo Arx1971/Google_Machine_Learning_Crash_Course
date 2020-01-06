f003ad4dc3fccf69b5ed74bdb4ac4612e8c59cf0
Arx1971 <adnan.rahin13@gmail.com>
2020-01-05 21:39:23 -0500

    Score: 0.75

--- linear_regression/__init__.py
+++ linear_regression/__init__.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv('../house_prices_train.csv')
-
-print(data.columns)
 
 features = [
     'MSSubClass',
     'LotArea',
-    'LotFrontage',
-    'LotShape',
     'OverallQual',
     'OverallCond',
     'BsmtUnfSF',
@@ -21,4 +19,14 @@ features = [
 y = data['SalePrice']
 X = data[features]
 
-print(X)
+train_data, test_data, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+x_train = train_data.fillna(train_data.mean)
+x_test = test_data.fillna(test_data.mean)
+
+linear_model = LinearRegression()
+model = linear_model.fit(x_train, y_train)
+
+prediction = linear_model.predict(x_test)
+print(prediction)
+print(linear_model.score(X, y))
